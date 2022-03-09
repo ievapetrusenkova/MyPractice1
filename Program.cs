cd22cfd826c51a2e84697a2b5dd6efd3c5768840
Ieva Petrusenkova <ieva.pilenge@gmail.com>
2022-03-09 16:04:42 +0200

    tasks

--- Program.cs
+++ Program.cs
@@ -1,88 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("\n\n");
-short A = 82;
-short B = 20;
-Console.WriteLine(Math.Max(A, B));
-Console.WriteLine(Math.Min(A, B));
-
-int newA = A;
-int newB = B;
-Console.Write("\n\n");
-Console.Write($"Check whether {A} is even or odd :\n");
-Console.Write("---------------------------------------");
-Console.Write("\n\n");
-Console.Write("Input an integer : ");
-newA = Convert.ToInt32(Console.ReadLine());
-newB = newA % 2;
-if (newB == 0)
-    Console.WriteLine($"{A} is an even integer.\n", newA);
-else
-    Console.WriteLine($"{A} is an odd integer.\n", newA);
-Console.Write("\n\n");
-Console.Write($"Check whether {B} is even or odd :\n");
-Console.Write("---------------------------------------");
-Console.Write("\n\n");
-Console.Write("Input an integer : ");
-newB = Convert.ToInt32(Console.ReadLine());
-newA = newB % 2;
-if (newA == 0)
-    Console.WriteLine($"{B} is an even integer.\n", newB);
-else
-    Console.WriteLine($"{B} is an odd integer.\n", newB);
-Console.Write("\n\n");
-Console.Write($"Check if {A} and {B} are equal :\n");
-if (newA == newB)
-    Console.WriteLine($"{A} and {B} are equal.\n");
-else
-    Console.WriteLine($"{A} and {B} are not equal.\n");
-if (newA >= 0)
-{
-    if (newB >= 0)
-    {
-        Console.WriteLine($"{A} and {B} are positive numbers.\n");
-    }
-}
-if (newA < 100)
-{
-    if (newB < 100)
-    {
-        Console.WriteLine($" {A} and {B} are less than 100");
-    }
-}
-Console.Write("\n\n");
-
-//int time = 8;
-//int Day = 5;
-//int Month = 11;
-//int Year = 2022;
-
-if (time < 12)
-{
-    Console.WriteLine("Good morning Sunshine!");
-}
-if (time > 13 && time < 19)
-{
-    Console.WriteLine("Good afternoon. Work hard.");
-}
-if (time > 20 && time < 24)
-{
-    Console.WriteLine("Good evening. Get some rest.");
-}
-// Day7, individual task1 Ieva P.
+﻿// Day7, individual task1 Ieva P.
 
 // Task 2 Ieva P. practice
-int day;
 
+var inputdate = Console.ReadLine();
+int day = Convert.ToInt32(inputdate);
+Console.WriteLine($"\n Write the day number:");
+Console.ReadLine();
 if (day <= 0 && day > 7)
 {
-    Console.WriteLine($"Write the day number:{day}");
-    Console.ReadLine();
-}
-else
-{
-    Console.WriteLine("Invalid input!");
+    Console.WriteLine("\nInvalid input!");
 }
     switch (day)
     {
@@ -91,19 +17,85 @@ else
         case 3:
         case 4:
         case 5:
-            Console.WriteLine("It is a working day");
+            Console.WriteLine("\nIt is a working day");
             break;
         case 6:
         case 7:
-            Console.WriteLine("It is holiday!");
+            Console.WriteLine("\nIt is holiday!");
             break;
         default:
-            Console.WriteLine("Invalid input!");
+            Console.WriteLine("\nInvalid input!");
             break;
     }
 
-    
-
+//Task 1
+var DayOfYear = Console.ReadLine();
+int Month=1;
+int Day = Convert.ToInt32(DayOfYear);
+Console.WriteLine($"\n Write the day number:");
+Console.ReadLine();
+if (Day <= 0 && Day > 365)
+{
+    Console.WriteLine("\nInvalid input!");
+}
+if (Month >=0 && Month >12)
+{
+    Console.WriteLine("\nInvalid input!");
+}
+switch (Month)
+{
+    case 1:
+       Day = (1 - 31);
+        Console.WriteLine("\n It is January");
+        break;
+    case 2:
+        Day = (32 - 59);
+        Console.WriteLine("\n It is February");
+        break;
+    case 3:
+        Day = (60 - 90);
+        Console.WriteLine("\n It is March");
+        break;
+    case 4:
+        Day = (91 - 120);
+        Console.WriteLine("\n It is April");
+        break;
+    case 5:
+        Day = (121 - 151);
+        Console.WriteLine("\nIt is May");
+        break;
+    case 6:
+        Day = (152 - 181);
+        Console.WriteLine("\nIt is June");
+        break;
+    case 7:
+        Day = (182 - 212);
+        Console.WriteLine("\nIt is July!");
+        break;
+    case 8:
+        Day = (213 - 243);
+        Console.WriteLine("\nIt is August!");
+        break;
+    case 9:
+        Day = (244 - 273);
+        Console.WriteLine("\nIt is September!");
+        break;
+    case 10:
+        Day = (274 - 304);
+        Console.WriteLine("\nIt is October!");
+        break;
+    case 11:
+        Day = (305 - 334);
+        Console.WriteLine("\nIt is November!");
+        break;
+    case 12:
+        Day = (335 - 365);
+        Console.WriteLine("\nIt is December!");
+        break;
+    default:
+        Console.WriteLine("\nInvalid input!");
+        break;
+}
 
 
 
